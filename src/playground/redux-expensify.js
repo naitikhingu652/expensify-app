884e209c0e37ca54de903221187afe51c34deab2
Naitik Hingu <naitikhingu652@gmail.com>
2020-05-03 17:59:02 +1000

    Reducers

---
+++ src/playground/redux-expensify.js
@@ -0,0 +1,11 @@
+import { createStore, combineReducers } from 'redux';
+
+const demoState = {
+  expenses: [{
+    id: '1',
+    description: 'Rent',
+    note: 'HAHAHAHA',
+    amount: 54500,
+    createdAt: 0
+  }]
+};
