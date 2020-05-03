a781854823795170c6b6b4a22652080aed175dbd
Naitik Hingu <naitikhingu652@gmail.com>
2020-05-03 16:55:14 +1000

    Dispathing Action to Redux Store

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -1,7 +1,30 @@
 import { createStore } from 'redux';
 
-const store = createStore((state = { count: 0 }) => {
- return state;
+const store = createStore((state = { count: 0 }, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return {
+        count: state.count + 1
+      };
+    case 'DECREMENT':
+      return {
+        count: state.count - 1
+      };
+    case 'RESET':
+      return {
+        count: 0
+      };
+    default:
+      return state;
+  }
+});
+
+store.dispatch({
+  type: 'DECREMENT'
+});
+
+store.dispatch({
+  type: 'RESET'
 });
 
 console.log(store.getState());
