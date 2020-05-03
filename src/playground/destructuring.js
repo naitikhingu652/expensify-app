7da390c7fbf1c2f5716fafb8f6a4c350c75929f4
Naitik Hingu <naitikhingu652@gmail.com>
2020-05-03 17:32:11 +1000

    ES6 Array Destructuring

--- src/playground/destructuring.js
+++ src/playground/destructuring.js
@@ -1,3 +1,7 @@
+//
+// Object Destructuring
+//
+
 const person = {
   name: 'Naitik',
   age: 24,
@@ -31,3 +35,16 @@ const book = {
 
 const { name: publisherName = 'Self-Published'} = book.publisher;
 console.log(publisherName);
+
+//
+// Array Destructuring
+//
+
+const address = ['U-309 8 Station Street', 'Sydney', 'NSW', '2140'];
+// console.log (`You are in ${address[1]}, ${address[2]}.`);
+const [ , city1, state = 'Australia'] = address;
+console.log(`You are in ${city1}, ${state}`);
+
+const item = ['Coffee (Hot)', '$2.00', '$2.50', "$3.00"];
+const [ itemName, , mediumPrice ] = item;
+console.log(`A medium ${itemName} costs ${mediumPrice}.`);
