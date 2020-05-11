e0800979df4bb138e795f04a604d5ed3c71f59af
Naitik Hingu <naitikhingu652@gmail.com>
2020-05-11 18:50:41 +1000

    Controlled Input for Filters

--- src/app.js
+++ src/app.js
@@ -4,7 +4,6 @@ import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import { addExpense } from './actions/expenses';
-import { setTextFilter } from './actions/filters';
 import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
@@ -13,11 +12,6 @@ const store = configureStore();
 
 store.dispatch(addExpense({ description: 'Water bill', amount: 4500 }));
 store.dispatch(addExpense({ description: 'Gas bill' }));
-store.dispatch(setTextFilter('water'));
-
-setTimeout(() => {
-  store.dispatch(setTextFilter('Bill'));
-}, 3000);
 
 const state = store.getState();
 console.log(getVisibleExpenses(state.expenses, state.filters));
