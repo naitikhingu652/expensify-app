d9a8c746756c1980b7a03a0ec98eeb757f8e25b9
Naitik Hingu <naitikhingu652@gmail.com>
2020-05-12 22:11:17 +1000

    Wiring up Add Expense

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -10,7 +10,8 @@ export default class ExpenseForm extends React.Component {
     note: '',
     amount: '',
     createdAt: moment(),
-    calendarFocused: false
+    calendarFocused: false,
+    error: ''
   };
 
   onDescriptionChange = (e) => {
@@ -25,23 +26,41 @@ export default class ExpenseForm extends React.Component {
 
   onAmountChange = (e) => {
     const amount = e.target.value;
-    if (amount.match(/^\d*(\.\d{0,2})?$/)) {
+    if (!amount || amount.match(/^\d{1,}(\.\d{0,2})?$/)) {
       this.setState(() => ({ amount }));
     }
   };
 
   onDateChange = (createdAt) => {
-    this.setState(() => ({ createdAt }));
+    if (createdAt) {
+      this.setState(() => ({ createdAt }));
+    }
   };
 
   onFocusChange = ({ focused }) => {
     this.setState(() => ({ calendarFocused: focused }));
   };
 
+  onSubmit = (e) => {
+    e.preventDefault();
+    if (!this.state.description || !this.state.amount) {
+      this.setState(() => ({ error: 'Please provide description and/or amount!' }));
+    } else {
+      this.setState(() => ({ error:'' }));
+      this.props.onSubmit({
+        description: this.state.description,
+        amount: Math.ceil(parseFloat(this.state.amount, 10) * 100),
+        createdAt: this.state.createdAt.valueOf(),
+        note: this.state.note
+      });
+    }
+  };
+
   render() {
     return (
       <div>
-        <form>
+        <p>{this.state.error}</p>
+        <form onSubmit={this.onSubmit}>
           <input
             type="text"
             placeholder="Description"
@@ -62,7 +81,7 @@ export default class ExpenseForm extends React.Component {
             focused={this.state.calendarFocused}
             onFocusChange={this.onFocusChange}
             numberOfMonths={1}
-            isOutsideRange={() => false }
+            isOutsideRange={() => false}
           />
           <textarea
             placeholder="Add a note for your expense(optional)"
