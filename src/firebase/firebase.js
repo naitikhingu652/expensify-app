cc70cab1a85acc93bb359d1decd3a1e04db1d3ca
Naitik Hingu <naitikhingu652@gmail.com>
2020-06-14 16:54:59 +1000

    Setting up Firebase

---
+++ src/firebase/firebase.js
@@ -0,0 +1,18 @@
+import * as firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCurPBykK_OAOpQdmf7ID5M4Swm29K4r9A",
+  authDomain: "expensify-72da6.firebaseapp.com",
+  databaseURL: "https://expensify-72da6.firebaseio.com",
+  projectId: "expensify-72da6",
+  storageBucket: "expensify-72da6.appspot.com",
+  messagingSenderId: "207911016215",
+  appId: "1:207911016215:web:e97cbbeb63057848328952",
+  measurementId: "G-3ZMZG60PF0"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+firebase.database().ref().set({
+  name: 'Naitik Hingu'
+});
