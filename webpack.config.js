3bec1322b1714288664a376937f4fb005337f343
Naitik Hingu <naitikhingu652@gmail.com>
2020-06-21 13:52:19 +1000

    typo fixed for dotenv config

--- webpack.config.js
+++ webpack.config.js
@@ -5,9 +5,9 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 if (process.env.NODE_ENV === 'text') {
-  require('dotenv').comfig({ path: '.env.test' });
+  require('dotenv').config({ path: '.env.test' });
 } else if (process.env.NODE_ENV === 'development') {
-  require('dotenv').comfig({ path: '.env.development' });
+  require('dotenv').config({ path: '.env.development' });
 }
 
 module.exports = (env) => {
