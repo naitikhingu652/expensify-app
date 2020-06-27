112e160f6538416cf4f45cea5fdcbc33899c7bd2
Naitik Hingu <naitikhingu652@gmail.com>
2020-06-27 17:45:17 +1000

    Styling Button and Header

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -7,7 +7,7 @@ export const LoginPage = ({ startLogin }) => (
     <div className="box-layout__box">
       <h1 className="box-layout__title">Let It Expensify</h1>
       <p>Take control of your expenses.</p>
-      <button onClick={startLogin}>Login</button>
+      <button className="button" onClick={startLogin}>Login with Google</button>
     </div>
   </div>
 );
